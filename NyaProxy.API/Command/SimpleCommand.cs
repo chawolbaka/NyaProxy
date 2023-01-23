89cbdab6ecc312d46ea946809d8526ae41d15cdf
chawol <chawolbaka@hotmail.com>
2023-01-24 02:36:01 +0800

    简化指令注册

---
+++ NyaProxy.API/Command/SimpleCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyaProxy.API
+{
+    public class SimpleCommand : Command
+    {
+        public override string Name { get; }
+
+        public override string Usage { get; }
+
+        public override string Description { get; }
+
+        private Func<string[], ICommandHelper, Task> _func;
+
+        public SimpleCommand(string commandName, Func<string[], ICommandHelper, Task> executeAsync) : this(commandName, "", "", executeAsync) { }
+        public SimpleCommand(string commandName, string commandUsage, Func<string[], ICommandHelper, Task> executeAsync) : this(commandName, commandUsage, "", executeAsync) { }
+        public SimpleCommand(string commandName, string commandUsage, string commandDescription, Func<string[], ICommandHelper, Task> executeAsync)
+        {
+            Name = commandName ?? throw new ArgumentNullException(nameof(commandName));
+            Usage = commandUsage ?? throw new ArgumentNullException(nameof(commandUsage));
+            Description = commandDescription ?? throw new ArgumentNullException(nameof(commandDescription));
+            _func = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
+        }
+
+        public override Task ExecuteAsync(string[] args, ICommandHelper helper)
+        {
+            return _func(args, helper);
+        }
+
+        public override IEnumerable<string> GetTabCompletions(ReadOnlySpan<string> args)
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
--- NyaProxy/CommandManager.cs
+++ NyaProxy/CommandManager.cs
@@ -8,10 +8,11 @@ namespace NyaProxy
 {
     public class CommandManager
     {
-        public SortedDictionary<string, (string Source, Command Commnad)> CommandDictionary { get; }
+        public SortedDictionary<string, (string Source, Command Commnad)> RegisteredCommands { get; }
+
         public CommandManager()
         {
-            CommandDictionary = new ();
+            RegisteredCommands = new ();
         }
 
         public CommandManager Register(string source, Command command)
@@ -19,26 +20,32 @@ namespace NyaProxy
             if (command == null)
                 throw new ArgumentNullException(nameof(command));
 
-            if (!CommandDictionary.ContainsKey(command.Name))
-                CommandDictionary.Add(command.Name, (source,command));
+            if (!RegisteredCommands.ContainsKey(command.Name))
+                RegisteredCommands.Add(command.Name, (source,command));
             else
                 NyaProxy.Logger.Error(i18n.Error.CommandRegistered.Replace("{CommandName}", command.Name));
 
             return this;
         }
+
+        public bool Unregister(string commandName)
+        {
+            return RegisteredCommands.Remove(commandName);
+        }
+
         public async Task<CommandManager> RunAsync(string commandName, string[] args, ICommandHelper helper)
         {
-            if (!CommandDictionary.ContainsKey(commandName))
+            if (!RegisteredCommands.ContainsKey(commandName))
             {
                 NyaProxy.Logger.UnpreformatColorfully(i18n.Error.CommandNotFound.Replace("{CommandName}", commandName));
             }
-            else if (CommandDictionary[commandName].Commnad.MinimumArgs > args.Length)
+            else if (RegisteredCommands[commandName].Commnad.MinimumArgs > args.Length)
             {
-                NyaProxy.Logger.UnpreformatColorfully(i18n.Error.CommandLeastRequired.Replace("{CommandName}", commandName, "{MinimumArgs}", CommandDictionary[commandName].Commnad.MinimumArgs));
+                NyaProxy.Logger.UnpreformatColorfully(i18n.Error.CommandLeastRequired.Replace("{CommandName}", commandName, "{MinimumArgs}", RegisteredCommands[commandName].Commnad.MinimumArgs));
             }
             else
             {
-                await CommandDictionary[commandName].Commnad.ExecuteAsync(args, helper);
+                await RegisteredCommands[commandName].Commnad.ExecuteAsync(args, helper);
             }
             return this;
         }
