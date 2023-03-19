6ffd4dd3c6aa24c60649a4ec69e30b6552db1650
chawol <chawolbaka@hotmail.com>
2023-03-20 01:28:07 +0800

    修复选项会被参数顺序影响

--- NyaProxy.Plugin/Motd/ConfigCommand.cs
+++ NyaProxy.Plugin/Motd/ConfigCommand.cs
@@ -15,13 +15,21 @@ namespace Motd
 ";
 
         private string _host;
+        private string _address;
         public ConfigCommand(Action reload)
         {
-            AddArgument(new Argument("reload", async (arg, helper) => reload()));
-            AddOption(new Option("host", async (option, helper) => _host = option.Value));
-            AddOption(new Option("generate", async (option, helper) => 
+            AddArgument(new Argument("reload", async (arg, helper)  => reload()));
+            AddOption(new Option("host", async (option, helper)     => _host = option.Value));
+            AddOption(new Option("generate", async (option, helper) => _address = option.Value));
+        }
+
+        public override async Task ExecuteAsync(ReadOnlyMemory<string> args, ICommandHelper helper)
+        {
+            _host = null; _address = null;
+            await base.ExecuteAsync(args, helper);
+            if(_address!=null)
             {
-                IPEndPoint endPoint = await NetworkUtils.GetIPEndPointAsync(option.Value);
+                IPEndPoint endPoint = await NetworkUtils.GetIPEndPointAsync(_address);
                 ServerListPing slp = new ServerListPing(endPoint);
                 MotdConfig config = new MotdConfig();
                 config.PingReply = await slp.SendAsync();
@@ -30,13 +38,8 @@ namespace Motd
                 string fileName = $"{config.Host}.{MotdPlugin.CurrentInstance.Helper.Config.DefaultFileType}";
                 await MotdPlugin.CurrentInstance.Helper.Config.SaveAsync(MotdPlugin.CurrentInstance.Helper.Config.Register(config, Path.Combine("Hosts", fileName)));
                 helper.Logger.Unpreformat("§aGenerate success.");
-            }));
-        }
+            }
 
-        public override Task ExecuteAsync(ReadOnlyMemory<string> args, ICommandHelper helper)
-        {
-            _host = null;
-            return base.ExecuteAsync(args, helper);
         }
 
     }
