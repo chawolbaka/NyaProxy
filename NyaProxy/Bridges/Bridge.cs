bc3824895fe17ce01da367812bf66d4d93d19567
chawol <chawolbaka@hotmail.com>
2023-04-26 18:12:12 +0800

    修复SLP流程可能无法正常完成

--- NyaProxy/Bridges/Bridge.cs
+++ NyaProxy/Bridges/Bridge.cs
@@ -8,18 +8,15 @@ using NyaProxy.API;
 using NyaProxy.EventArgs;
 using NyaProxy.Extension;
 using NyaProxy.Configs;
-using Ionic.Zlib;
 using NyaProxy.Debug;
+using System.Threading.Tasks;
 
 namespace NyaProxy.Bridges
 {
     public abstract class Bridge : IBridge
     {
         internal static long Count => Interlocked.Read(ref _count);
-        private static long _count = 0;
-
         internal static long CurrentSequence => Interlocked.Read(ref _sequence);
-        private static long _sequence = 1000; //从0开始的话StringTable那边看着有点太短，这边设置成1000仅仅是为了看着舒服点。
 
         public long SessionId { get; }
         public Host Host { get; }
@@ -31,10 +28,13 @@ namespace NyaProxy.Bridges
         protected virtual string BreakMessage => $"{GetType().Name} breaked ({Source._remoteEndPoint()}<->{Destination._remoteEndPoint()})";
 
         private SpinLock _breakLock = new SpinLock();
-        private bool _isBreaked;
+        private bool _isBreaked; 
+        private static long _count = 0;
+        private static long _sequence = 1000; //从0开始的话StringTable那边看着有点太短，这边设置成1000仅仅是为了看着舒服点。
 
         public Bridge(Host host, string handshakeAddress, Socket source, Socket destination)
         {
+            Interlocked.CompareExchange(ref _sequence, 1000, int.MaxValue);
             SessionId = Interlocked.Increment(ref _sequence);
             HandshakeAddress = handshakeAddress;
             Source = source ?? throw new ArgumentNullException(nameof(source));
@@ -74,16 +74,22 @@ namespace NyaProxy.Bridges
                 if (!NyaProxy.Bridges.ContainsKey(SessionId))
                     return;
                 ListenToken?.Cancel();
-                if (Source is not null && NetworkUtils.CheckConnect(Source))
+                Task.Run(async () => 
                 {
-                    Source.Shutdown(SocketShutdown.Both);
-                    Source.Close();
-                }
-                if (Destination is not null && NetworkUtils.CheckConnect(Destination))
-                {
-                    Destination.Shutdown(SocketShutdown.Both);
-                    Destination.Close();
-                }
+                    //直接断开socket可能会导致有遗言发不出来。
+                    await Task.Delay(1000);
+                    if (Source is not null && NetworkUtils.CheckConnect(Source))
+                    {
+                        Source.Shutdown(SocketShutdown.Both);
+                        Source.Close();
+                    }
+                    if (Destination is not null && NetworkUtils.CheckConnect(Destination))
+                    {
+                        Destination.Shutdown(SocketShutdown.Both);
+                        Destination.Close();
+                    }
+                });
+        
                 NyaProxy.Disconnected.Invoke(this, new DisconnectEventArgs(Host.Name));
                 NyaProxy.Logger.Info(BreakMessage);
             }
@@ -99,12 +105,12 @@ namespace NyaProxy.Bridges
         protected virtual void SimpleExceptionHandle(object sender, UnhandledIOExceptionEventArgs e)
         {
             e.Handled = true;
-            if ((e.Exception is ZlibException || e.Exception is OverflowException) && NetworkUtils.CheckConnect(Source) && NetworkUtils.CheckConnect(Destination))
+            if (e.Exception is OverflowException && NetworkUtils.CheckConnect(Source) && NetworkUtils.CheckConnect(Destination))
             {
                 NyaProxy.Logger.Error(i18n.Error.PacketDecompressFailed);
                 return;
             }
-            else if (e.Exception is not ObjectDisposedException && e.Exception is not SocketException && e.Exception is not ZlibException && e.Exception is not OverflowException)
+            else if (e.Exception is not ObjectDisposedException && e.Exception is not SocketException && e.Exception is not OverflowException)
             {
                 Crash.Report(e.Exception, true, true, false);
                 Break();
