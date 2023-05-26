453efb0b239b3043285d89ec0759373de7e92fb4
chawol <chawolbaka@hotmail.com>
2023-05-26 18:16:54 +0800

    添加不显示所有时间的选项

--- NyaProxy.Plugin/Analysis/Commands/PlayCommand.cs
+++ NyaProxy.Plugin/Analysis/Commands/PlayCommand.cs
@@ -1,4 +1,5 @@
-﻿using NyaProxy.API.Command;
+﻿using NyaProxy.API;
+using NyaProxy.API.Command;
 using StringTable;
 
 namespace Analysis.Commands
@@ -9,16 +10,21 @@ namespace Analysis.Commands
 
         public override int MinimumArgs => 0;
 
-        public virtual bool HideTime { get; set; }
+        public virtual bool ShowShortTime { get; set; }
+
+        public virtual bool ShowFullTime { get; set; }
 
         public PlayCommand()
         {
-            AddOption(new Option("--show-time", 0, (command, e) => HideTime = false));
+            AddOption(new Option("--show-time", 0, (command, e) => ShowShortTime = true));
+
+            AddOption(new Option("--show-time-full", 0, (command, e) => ShowFullTime = true));
         }
 
         public override async Task<bool> ExecuteAsync(ReadOnlyMemory<string> args, ICommandHelper helper)
         {
-            HideTime = true;
+            ShowShortTime = false;
+            ShowFullTime = false;
 
             if (await base.ExecuteAsync(args, helper))
             {
@@ -26,10 +32,40 @@ namespace Analysis.Commands
                 {
                     StringTableBuilder table = new StringTableBuilder();
                     table.AddColumn("Id", "Host", "Player", "Client", "Server", "Transferred");
-                    if (!HideTime)
+                    if (ShowFullTime)
                         table.AddColumn("Connect", "Handshake", "LoginStart", "LoginSuccess", "Disconnect");
+                    else if (ShowShortTime)
+                        table.AddColumn("Connect", "Disconnect");
+
+
                     foreach (var session in AnalysisData.Sessions.Values)
-                        table.AddRow(session.ToRow(HideTime));
+                    {
+                        List<object> row = new List<object>
+                        { 
+                            session.SessionId,
+                            session.Host != null ? session.Host.Name : "",
+                            session.Player != null ? session.Player.Name : "",
+                            session.Source != null ? session.Source.ToString() : "",
+                            session.Destination != null ? session.Destination.ToString() : "",
+                            Utils.SizeSuffix(session.PacketAnalysis.Client.BytesTransferred+session.PacketAnalysis.Server.BytesTransferred)};
+
+                        
+                        if (ShowFullTime)
+                        {
+                            row.AddRange(new object[] {
+                                session.ConnectTime      != default ? session.ConnectTime      : "",
+                                session.HandshakeTime    != default ? session.HandshakeTime    : "",
+                                session.LoginStartTime   != default ? session.LoginStartTime   : "",
+                                session.LoginSuccessTime != default ? session.LoginSuccessTime : "",
+                                session.DisconnectTime   != default ? session.DisconnectTime   : ""});
+                        }
+                        else if (ShowShortTime)
+                        {
+                            row.AddRange(new object[] {
+                                session.ConnectTime      != default ? session.ConnectTime      : "",
+                                session.DisconnectTime   != default ? session.DisconnectTime   : ""});
+                        }
+                    }
 
                     helper.Logger.Unpreformat(table.Export());
                 }
--- NyaProxy.Plugin/Analysis/SessionAnalysisData.cs
+++ NyaProxy.Plugin/Analysis/SessionAnalysisData.cs
@@ -18,31 +18,5 @@ namespace Analysis
         {
             PacketAnalysis = (new PacketAnalysisData(), new PacketAnalysisData());
         }
-
-        public List<object> ToRow(bool hideTime)
-        {
-            List<object> row = new List<object>
-            {
-                SessionId,
-                Host != null ? Host.Name : "",
-                Player != null ? Player.Name : "",
-                Source != null ? Source.ToString() : "",
-                Destination != null ? Destination.ToString() : "",
-                Utils.SizeSuffix(PacketAnalysis.Client.BytesTransferred+PacketAnalysis.Server.BytesTransferred),
-            };
-
-            if (!hideTime)
-            {
-                row.AddRange(new object[] {
-                ConnectTime      != default ? ConnectTime      : "",
-                HandshakeTime    != default ? HandshakeTime    : "",
-                LoginStartTime   != default ? LoginStartTime   : "",
-                LoginSuccessTime != default ? LoginSuccessTime : "",
-                DisconnectTime   != default ? DisconnectTime   : ""
-                });
-            }
-            return row;
-        }
-
     }
 }
